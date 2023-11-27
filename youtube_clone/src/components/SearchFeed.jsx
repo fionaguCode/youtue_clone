eb286018f4b58de466a378cb3cf3df9e64587962
Fiona <fanghua9853@csu.fullerton.edu>
2023-11-27 01:43:43 -0800

    search bar, video details

--- youtube_clone/src/components/SearchFeed.jsx
+++ youtube_clone/src/components/SearchFeed.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import {  Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 export const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => setVideos(data.items))
+  },[searchTerm]);
+  
+  
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ padding:3,overflowY: "auto", height: "90vh", flex: 2 }}>
+       <Typography variant="h4" fontWeight="bold" mb={4} sx={{ color: "white" }}>
+            Search Results for: <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+       </Typography>
+       <Videos videos={videos} />
+    </Box>
   )
 }
